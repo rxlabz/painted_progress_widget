d2b019861dbde151d80e23d3b6d537db238be9e5
rxlabz <dev@rxlabz.com>
2018-04-25 21:57:56 +0200

    step1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-
-import 'progress_button.dart';
 
 void main() => runApp(new App());
 
@@ -10,15 +6,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: ProgressButtonDemo(),
       ),
     );
   }
 }
-
-const double kStep = 2.0;
 
 class ProgressButtonDemo extends StatefulWidget {
   @override
@@ -27,30 +20,13 @@ class ProgressButtonDemo extends StatefulWidget {
 
 class _ProgressButtonDemoState extends State<ProgressButtonDemo> {
   double progress = 0.0;
-  Timer timer;
-
-  void start() {
-    if (progress == 100.0)
-      setState(() => progress = 0.0);
-    else
-      timer = Timer.periodic(Duration(milliseconds: 50), (t) {
-        if (progress == 100.0)
-          t.cancel();
-        else
-          setState(() => progress = progress + kStep);
-      });
-  }
 
   @override
   Widget build(BuildContext context) {
     return new Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        ProgressButton(
-          key: Key('progressButton'),
-          percentProgress: progress,
-          onPressed: start,
-        ),
+        Text("${progress.round()} %"),
         Slider(
           value: progress,
           min: 0.0,
@@ -59,11 +35,5 @@ class _ProgressButtonDemoState extends State<ProgressButtonDemo> {
         )
       ],
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (timer != null) timer.cancel();
   }
 }
